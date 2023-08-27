24efd9718e2bcd1b86ec62cd1cd166c546b0d414
md-alif-jasim <mdalifjasim@gmail.com>
2023-08-27 05:37:40 -0400

    chars added

--- 003/Main.java
+++ 003/Main.java
@@ -1,5 +1,15 @@
 public class Main{
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        char a=83;
+        char b=117;
+        char c=115;
+        char d=104;
+        char e=105;
+
+
+
+
     }
 }
