31d63e19a0c24cf704b10b74d1666e0d1d2bc329
md-alif-jasim <mdalifjasim@gmail.com>
2023-08-27 05:41:32 -0400

    chars printed

--- 003/Main.java
+++ 003/Main.java
@@ -2,12 +2,17 @@ public class Main{
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        char a=83;
-        char b=117;
-        char c=115;
-        char d=104;
-        char e=105;
+        char a=72;
+        char b=69;
+        char c=76;
+        char d=76;
+        char e=79;
 
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
 
 
 
